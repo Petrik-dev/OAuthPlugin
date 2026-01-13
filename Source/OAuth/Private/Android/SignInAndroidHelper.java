023dd79b060de36ca7277e5e9fd4ab596dd71be6
Daniil <_NOREPLY_EMAIL>
2026-01-13 22:21:25 +0100

    Creating the first Java helper (Hello World)

---
+++ Source/OAuth/Private/Android/SignInAndroidHelper.java
@@ -0,0 +1,14 @@
+package com.Plugin.SignInAndroidHelper;
+
+import android.util.Log;
+import androidx.annotation.Keep;
+
+@Keep
+public class SignInAndroidHelper
+{
+    public static String helloFromAndroid() 
+    {
+        Log.d("HelloBridge", "helloFromAndroid");
+        return "HelloFromAndroid";
+    }
+}
